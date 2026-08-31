276a36e7b67cf6547dd22ea254f11f109be459e5
innr <82075749+innr@users.noreply.github.com>
2026-08-31 21:41:16 +0800

    PR1: add protocol/__init__.py

---
+++ protocol/__init__.py
@@ -0,0 +1,4 @@
+from .model import Book, Chapter, TransferTask
+from .transfer import VirtualTransferEndpoint, TransferError
+
+__all__ = ["Book", "Chapter", "TransferTask", "VirtualTransferEndpoint", "TransferError"]
